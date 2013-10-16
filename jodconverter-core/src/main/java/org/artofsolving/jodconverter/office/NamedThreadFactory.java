dc9d0d7829f4aae6df0410c39fd3dc695aaf5ad9
Rubén Bressler Camps <ruben.bressler@datys.cu>
2013-10-16 18:47:14 -0400

    Cambiando a versión estable

--- jodconverter-core/src/main/java/org/artofsolving/jodconverter/office/NamedThreadFactory.java
+++ jodconverter-core/src/main/java/org/artofsolving/jodconverter/office/NamedThreadFactory.java
@@ -20,24 +20,25 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 class NamedThreadFactory implements ThreadFactory {
 
-    private static final AtomicInteger threadIndex = new AtomicInteger(0);
+	private static final AtomicInteger threadIndex = new AtomicInteger(0);
 
-    private final String baseName;
-    private final boolean daemon;
+	private final String baseName;
+	private final boolean daemon;
 
-    public NamedThreadFactory(String baseName) {
-        this(baseName, true);
-    }
+	public NamedThreadFactory(String baseName) {
+		this(baseName, true);
+	}
 
-    public NamedThreadFactory(String baseName, boolean daemon) {
-        this.baseName = baseName;
-        this.daemon = daemon;
-    }
+	public NamedThreadFactory(String baseName, boolean daemon) {
+		this.baseName = baseName;
+		this.daemon = daemon;
+	}
 
-    public Thread newThread(Runnable runnable) {
-        Thread thread = new Thread(runnable, baseName + "-" + threadIndex.getAndIncrement());
-        thread.setDaemon(daemon);
-        return thread;
-    }
+	public Thread newThread(Runnable runnable) {
+		Thread thread = new Thread(runnable, baseName + "-"
+		        + threadIndex.getAndIncrement());
+		thread.setDaemon(daemon);
+		return thread;
+	}
 
 }
